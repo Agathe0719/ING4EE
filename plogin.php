3a529fe6f4d6573f550c69f04a531b4a8c5584ef
Agathe0719 <61595937+Agathe0719@users.noreply.github.com>
2022-03-10 23:46:40 +0100

    cophp

---
+++ plogin.php
@@ -0,0 +1,47 @@
+<!-- Verif que la connection fonctionne bien -->
+
+<?php
+$nbline='a';
+    if(isset($_POST['submitConnexion']))
+    {
+        include("connexion.php");
+        session_start();
+        $nomConnexion = $_POST['nomConnexion'];
+        $Password = $_POST['password'];
+
+        // Pour tchequer toutes les lignes de la tab user
+        //for($int i =0; $i<15; $i++)
+    echo 'lolo';
+    $query = "SELECT COUNT(*) FROM user WHERE Company_Name='$nomConnexion' ";
+    $nbline = $con ->query($query);
+     // $nbline=mysqli_query($con, "SELECT COUNT(*) FROM user WHERE Company_Name='$nomConnexion' ");
+    echo '<br>lulu</br>';
+    
+      echo $nbline;
+      echo $query;
+    
+}
+    
+echo 'lalala';
+
+// Comparer tous les companyName et password avec ceux rentrés par l'utilisateur
+
+
+
+// Si la connection est effective, passage à la page suivante, page d'accueil
+
+if($nbline==1)
+    {
+    echo "<script language='javascript' type='text/javascript'> location.href='pinscription.html'</script>";
+    
+}
+
+//Si pas effective, message - Le mdp ou l'utilisateur est incorrect
+else
+{
+  echo "<p>MDP or user name incorrect </p>";
+  echo"<br> If you want you can see the website without connexion </br>";
+}
+
+
+?>
