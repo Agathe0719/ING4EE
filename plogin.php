b3a587c2c0c2295c810c30ac1a0cef3fe238cf9d
Agathe0719 <61595937+Agathe0719@users.noreply.github.com>
2022-03-12 13:45:20 +0100

    validationlogin

--- plogin.php
+++ plogin.php
@@ -2,6 +2,7 @@
 
 <?php
 $nbline=0;
+$compteur=3;
     if(isset($_POST['submitConnexion']))
     {
         include("connexion.php");
@@ -9,44 +10,34 @@ $nbline=0;
         $nomConnexion = $_POST['nomConnexion'];
         $Password = $_POST['password'];
 
-        // Pour tchequer toutes les lignes de la tab user
-        //for($int i =0; $i<15; $i++)
-    echo 'lolo';
-    //$query = "SELECT COUNT(*) FROM user WHERE Company_Name='$nomConnexion' ";
-    //$nbline = $con ->query($query);
-
-    $req=mysqli_query($con, "SELECT COUNT(*) as cpt FROM user WHERE Company_Name='$nomConnexion' ");
-    echo mysql_result($req,0);
-    $nbline = $req->fetch();
-    echo $nbline['cpt'];
-    
-    echo '<br>lulu</br>';
-    
-      echo $nbline;
-      echo $query;
-    
-}
-    
-echo 'lalala';
-
-// Comparer tous les companyName et password avec ceux rentrés par l'utilisateur
-
-
-
+//Si jamais le mdp et l'id correspondent au sql :
+        $sql = mysqli_query($con, "SELECT * FROM user WHERE Company_Name = '$nomConnexion' and Password = '$Password'");
+        $nbline = mysqli_num_rows($sql);
+        
+      echo $nbline;  
+      echo $compteur;
+  
 // Si la connection est effective, passage à la page suivante, page d'accueil
 
 if($nbline==1)
     {
-    echo "<script language='javascript' type='text/javascript'> location.href='pinscription.html'</script>";
-    
-}
+    $compteur=0;
+    //echo "<script language='javascript' type='text/javascript'> location.href='pinscription.html'</script>";
+    echo 't as reussi ptn de merde';   
+    }
 
 //Si pas effective, message - Le mdp ou l'utilisateur est incorrect
 else
 {
+  $compteur=$compteur-1;
+  
+ echo "<script language='javascript' type='text/javascript'> location.href='plogin.html'</script>";
   echo "<p>MDP or user name incorrect </p>";
-  echo"<br> If you want you can see the website without connexion </br>";
+
+  if ($compteur=0)
+  {echo "<script language='javascript' type='text/javascript'> location.href='pinscription.html'</script>";}
 }
 
+}
 
 ?>
